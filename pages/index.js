765229950c3fdef328fd7b4ee7a285e132ebeb68
Eric Florenzano <floguy@gmail.com>
2017-07-19 21:54:23 -0700

    Add contents of website

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+export default () => (
+  <div>
+    <p>Welcome to Maxint LLC. Existing projects include virtual reality software and game development, projects in development involve virtual reality content incentivization.</p>
+    <style jsx>{`
+    div {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 300px;
+    }
+    p {
+      width: 300px;
+      margin: 5px;
+      text-align: center;
+    }
+    `}</style>
+  </div>
+)
